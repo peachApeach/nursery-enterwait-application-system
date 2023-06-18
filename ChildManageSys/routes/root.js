19e8b3f35fe659e43be3f7b0d4e9ee58b3a9ca75
Sungah Park <sonia.0903.park@gmail.com>
2023-06-18 12:53:03 +0900

    Update: Get list from nur_child

--- ChildManageSys/routes/root.js
+++ ChildManageSys/routes/root.js
@@ -2,6 +2,15 @@
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    return { root: true }
+    try {
+      const mysql = fastify.mysql;
+      const query = `SELECT * FROM nur_child`;
+      const result = await mysql.query(query);
+      console.log(result);
+      reply.code(200).header('Content-type','application/json').send(result[0])
+    } catch (error) {
+      console.error(error);
+      reply.code(400).send("목록 조회에 실패했습니다.")
+    }
   })
 }
