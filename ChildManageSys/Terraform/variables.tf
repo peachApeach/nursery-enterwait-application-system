4d3b61ae9d1e4085e9d8b98844da5ef15209a3db
SH8432 <wendysh8432@gamil.com>
2023-06-23 15:33:23 +0900

    DELETE: terraform file

--- ChildManageSys/Terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "db_username" {
-  type        = string
-  description = "Username for the database"
-}
-
-variable "db_password" {
-  type        = string
-  description = "Password for the database"
-}
