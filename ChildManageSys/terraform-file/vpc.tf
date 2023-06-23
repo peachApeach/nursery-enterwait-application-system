754973996b544ac37c8c7b62c2c8019bc4f5018b
SH8432 <wendysh8432@gamil.com>
2023-06-23 11:57:06 +0900

    DELETE: childmanagesys-ECS-terraform file

--- ChildManageSys/terraform-file/vpc.tf
+++
@@ -1,9 +0,0 @@
-#AWS VPC 생성
-resource "aws_vpc" "final-project-ecs-vpc" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
-
-  tags = {
-    Name = "final-project-ecs-vpc"
-  }
-}
