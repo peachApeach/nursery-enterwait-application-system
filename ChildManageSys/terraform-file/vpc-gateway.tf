a7ca0e01941c58f17966655ec7c737abe6e16583
SH8432 <wendysh8432@gamil.com>
2023-06-23 13:50:47 +0900

    DELETE: childmanagesys-ECS-terraform file

--- ChildManageSys/terraform-file/vpc-gateway.tf
+++
@@ -1,8 +0,0 @@
-#IGW 생성
-resource "aws_internet_gateway" "final-project-ecs-IGW" {
-  vpc_id = aws_vpc.final-project-ecs-vpc.id
-
-  tags = {
-    Name = "final-project-ecs-internet-gateway"
-  }
-}
