ff93335055dd95a48949a0a55f23c755c5146889
jackykim <jackykim93@github.com>
2023-06-24 01:37:34 +0900

    FIX: security-group.tf

--- ChildManageSys/Terraform/security-group.tf
+++ ChildManageSys/Terraform/security-group.tf
@@ -1,4 +1,3 @@
-#public 보안 그룹 생성
 resource "aws_security_group" "final-project-ecs-public-security-group" {
   vpc_id = aws_vpc.final-project-ecs-vpc.id
 
@@ -6,7 +5,7 @@ resource "aws_security_group" "final-project-ecs-public-security-group" {
     Name = "final-project-ecs-public-security-group"
   }
 }
-#private 보안 그룹 생성
+
 resource "aws_security_group" "final-project-ecs-private-security-group" {
   vpc_id = aws_vpc.final-project-ecs-vpc.id
 
@@ -14,7 +13,26 @@ resource "aws_security_group" "final-project-ecs-private-security-group" {
     Name = "final-project-ecs-private-security-group"
   }
 }
-#public 보안그룹 ssh 인바운드 규칙 생성
+
+resource "aws_security_group" "rds-ecs-sg" {
+  name = "rds-ecs-sg"
+  vpc_id = aws_vpc.final-project-ecs-vpc.id
+  
+  ingress {
+    from_port = 3306
+    to_port = 3306
+    protocol = "tcp"
+    security_groups = [aws_security_group.final-project-ecs-private-security-group.id]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_security_group_rule" "final-project-ecs-public-security-group-rule1" {
   type = "ingress"
   from_port = 22
@@ -23,7 +41,7 @@ resource "aws_security_group_rule" "final-project-ecs-public-security-group-rule
   cidr_blocks = ["0.0.0.0/32"]
   security_group_id = aws_security_group.final-project-ecs-public-security-group.id
 }
-#public 보안그룹 http 인바운드 규칙 생성
+
 resource "aws_security_group_rule" "final-project-ecs-public-security-group-rule2" {
   type = "ingress"
   from_port = 80
@@ -32,7 +50,7 @@ resource "aws_security_group_rule" "final-project-ecs-public-security-group-rule
   cidr_blocks = ["0.0.0.0/0"]
   security_group_id = aws_security_group.final-project-ecs-public-security-group.id
 }
-#public 보안그룹 https 인바운드 규칙 생성
+
 resource "aws_security_group_rule" "final-project-ecs-public-security-group-rule3" {
   type = "ingress"
   from_port = 443
@@ -41,7 +59,7 @@ resource "aws_security_group_rule" "final-project-ecs-public-security-group-rule
   cidr_blocks = ["0.0.0.0/0"]
   security_group_id = aws_security_group.final-project-ecs-public-security-group.id
 }
-#public 아웃바운드 규칙 생성
+
 resource "aws_security_group_rule" "final-project-ecs-public-security-group-outboundrule" {
   type = "egress"
   from_port = 0
@@ -50,7 +68,7 @@ resource "aws_security_group_rule" "final-project-ecs-public-security-group-outb
   cidr_blocks = ["0.0.0.0/0"]
   security_group_id = aws_security_group.final-project-ecs-public-security-group.id
 }
-#private mysql DB 인바운드 규칙 생성
+
 resource "aws_security_group_rule" "final-project-ecs-private-security-group-rule1" {
   type = "ingress"
   from_port = 3306
@@ -59,7 +77,7 @@ resource "aws_security_group_rule" "final-project-ecs-private-security-group-rul
   cidr_blocks = ["0.0.0.0/0"]
   security_group_id = aws_security_group.final-project-ecs-private-security-group.id
 }
-#private 아웃바운드 규칙 생성
+
 resource "aws_security_group_rule" "final-project-ecs-private-security-group-outboundrule" {
   type = "egress"
   from_port = 0
@@ -68,6 +86,32 @@ resource "aws_security_group_rule" "final-project-ecs-private-security-group-out
   cidr_blocks = ["0.0.0.0/0"]
   security_group_id = aws_security_group.final-project-ecs-private-security-group.id
 }
+
+resource "aws_security_group" "lb" {
+  vpc_id = aws_vpc.final-project-ecs-vpc.id
+  name   = "lb-sg"
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 #ECS task http 보안그룹 인바운드, 아웃바운드 규칙 생성
 resource "aws_security_group" "ecs-tasks-sg" {
   vpc_id = aws_vpc.final-project-ecs-vpc.id
@@ -77,6 +121,22 @@ resource "aws_security_group" "ecs-tasks-sg" {
     protocol        = "tcp"
     from_port       = 80
     to_port         = 80
+    cidr_blocks     = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.final-project-ecs-public-security-group.id]
+  }
+
+ingress {
+    protocol        = "tcp"
+    from_port       = 22
+    to_port         = 22
+    cidr_blocks     = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.final-project-ecs-public-security-group.id]
+  }
+
+ingress {
+    protocol        = "tcp"
+    from_port       = 4000
+    to_port         = 4000
     cidr_blocks     = ["0.0.0.0/0"]
     security_groups = [aws_security_group.final-project-ecs-public-security-group.id]
   }
