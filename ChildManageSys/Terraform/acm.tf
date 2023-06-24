99e4be63bcb12e07a50274f5f80946869872bc73
SH8432 <wendysh8432@gamil.com>
2023-06-25 02:58:13 +0900

    FEAT: route53 A record

--- ChildManageSys/Terraform/acm.tf
+++ ChildManageSys/Terraform/acm.tf
@@ -4,10 +4,3 @@ data "aws_acm_certificate" "love-liverpool-certificate" {
   statuses = ["ISSUED"]
   types    = ["AMAZON_ISSUED"]
 }
-#AWS route53의 도메인을 참조하는 코드
-data "aws_route53_zone" "love-liverpool-domain" {
-  name = var.domain
-}
-
-# 레코드 생성 테라폼 코드
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/route53_zone
