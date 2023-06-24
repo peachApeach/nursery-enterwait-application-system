75fb3a918410499bea24c884feddf1463999aba5
Sungah Park <107600263+sonia0903@users.noreply.github.com>
2023-06-25 01:48:42 +0900

    FIX: ecs-lb.tf port 4000

--- ChildManageSys/Terraform/ecs-lb.tf
+++ ChildManageSys/Terraform/ecs-lb.tf
@@ -51,7 +51,7 @@ resource "aws_lb_listener" "http_forward" {
 #ALB의 타겟 그룹 생성
 resource "aws_lb_target_group" "final-project-ecs-service-alb-tg" {
   name        = "final-project-ecs-service-alb-tg"
-  port        = 80
+  port        = 4000
   protocol    = "HTTP"
   vpc_id      = aws_vpc.final-project-ecs-vpc.id
   target_type = "ip"
