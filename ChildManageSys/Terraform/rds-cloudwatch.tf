4a908c19e286d9d842f392e1b16e8e80aaca1dc1
SH8432 <wendysh8432@gamil.com>
2023-06-27 16:22:16 +0900

    FIX: merge terraform file

--- ChildManageSys/Terraform/rds-cloudwatch.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_cloudwatch_log_group" "rds-cloudwatch" {
-  name = "rds-cloudwatch"
-}
-
-resource "aws_cloudwatch_log_stream" "rds-cloudwatch-log" {
-  name           = "rds-cloudwatch-log"
-  log_group_name = aws_cloudwatch_log_group.rds-cloudwatch.name
-}
